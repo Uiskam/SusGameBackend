6a56c4905c2721643ac8107670c48d4640482227
Nepommuck <pawel.konopka5@gmail.com>
2024-10-13 16:52:27 +0200

    Bump DTO version

--- src/main/kotlin/edu/agh/susgame/back/models/dto/rest/ApiResult.kt
+++ src/main/kotlin/edu/agh/susgame/back/models/dto/rest/ApiResult.kt
@@ -3,15 +3,6 @@
 // IN ORDER TO CHANGE THIS DTO, COMMIT TO 'SusGameDTO' GITHUB REPOSITORY
 // IN ORDER TO UPDATE THIS FILE TO NEWEST VERSION, RUN 'scripts/update-DTO.sh'
 
-package edu.agh.susgame.dto.rest.model
+package edu.agh.susgame.dto.rest
 
-
-data class LobbyId(val value: Int)
-
-data class Lobby(
-    val id: LobbyId,
-    val name: String,
-    val maxNumOfPlayers: Int,
-    val gameTime: Int,
-    val playersWaiting: Map<PlayerId, Player> = emptyMap(),
-)
+abstract class ApiResult(val responseCode: Int)
--- src/main/kotlin/edu/agh/susgame/back/models/dto/rest/games/model/Result.kt
+++ src/main/kotlin/edu/agh/susgame/back/models/dto/rest/games/model/Result.kt
@@ -5,23 +5,25 @@
 
 package edu.agh.susgame.dto.rest.games.model
 
+import edu.agh.susgame.dto.rest.ApiResult
 import edu.agh.susgame.dto.rest.model.Lobby
 import edu.agh.susgame.dto.rest.model.LobbyId
+import java.net.HttpURLConnection
 
 
-sealed class GetAllGamesApiResult {
-    data class Success(val lobbies: List<Lobby>) : GetAllGamesApiResult()
-    data object Error : GetAllGamesApiResult()
+sealed class GetAllGamesApiResult(responseCode: Int) : ApiResult(responseCode) {
+    data class Success(val lobbies: List<Lobby>) : GetAllGamesApiResult(HttpURLConnection.HTTP_OK)
+    data object Error : GetAllGamesApiResult(HttpURLConnection.HTTP_INTERNAL_ERROR)
 }
 
-sealed class GetGameApiResult {
-    data class Success(val lobby: Lobby) : GetGameApiResult()
-    data object DoesNotExist : GetGameApiResult()
-    data object OtherError : GetGameApiResult()
+sealed class GetGameApiResult(responseCode: Int) : ApiResult(responseCode) {
+    data class Success(val lobby: Lobby) : GetGameApiResult(HttpURLConnection.HTTP_OK)
+    data object DoesNotExist : GetGameApiResult(HttpURLConnection.HTTP_NOT_FOUND)
+    data object OtherError : GetGameApiResult(HttpURLConnection.HTTP_INTERNAL_ERROR)
 }
 
-sealed class CreateGameApiResult {
-    data class Success(val createdLobbyId: LobbyId) : CreateGameApiResult()
-    data object NameAlreadyExists : CreateGameApiResult()
-    data object OtherError : CreateGameApiResult()
+sealed class CreateGameApiResult(responseCode: Int) : ApiResult(responseCode) {
+    data class Success(val createdLobbyId: LobbyId) : CreateGameApiResult(HttpURLConnection.HTTP_CREATED)
+    data object NameAlreadyExists : CreateGameApiResult(HttpURLConnection.HTTP_CONFLICT)
+    data object OtherError : CreateGameApiResult(HttpURLConnection.HTTP_INTERNAL_ERROR)
 }
