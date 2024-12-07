cfc52aa60aff08b75fe92d09e31d660869683fc9
uqsz <uqsz123@gmail.com>
2024-12-07 20:00:06 +0100

    missing serializable

--- src/main/kotlin/edu/agh/susgame/dto/rest/games/model/Response.kt
+++ src/main/kotlin/edu/agh/susgame/dto/rest/games/model/Response.kt
@@ -6,8 +6,9 @@
 package edu.agh.susgame.dto.rest.games.model
 
 import edu.agh.susgame.dto.rest.model.LobbyId
+import kotlinx.serialization.Serializable
 
-
+@Serializable
 data class GameCreationApiResponse(
     val lobbyId: LobbyId,
 )
