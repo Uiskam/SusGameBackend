3c8bf1c3b59d4a8b497c9c89b465f9600a08fb7c
Uiskam <maksulima@gmail.com>
2024-12-19 18:18:10 +0100

    New maps

--- src/main/kotlin/edu/agh/susgame/back/domain/build/GameInitializer.kt
+++ src/main/kotlin/edu/agh/susgame/back/domain/build/GameInitializer.kt
@@ -9,7 +9,9 @@ object GameInitializer {
     fun getGameParams(players: List<Player>): GameConfig {
         val directoryPath = "src/main/resources/game_files/${players.size}"
         val directory = File(directoryPath)
-        val files = directory.listFiles { _, name -> name.endsWith(".json") }
+        var files = directory.listFiles { _, name -> name.endsWith(".json") }
+        files = directory.listFiles { _, name -> name.endsWith(".json") && name.contains("new") }
+
 
         val randomFile = if (files != null && files.isNotEmpty()) {
             files[Random.nextInt(files.size)]
