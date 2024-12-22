a1aa7151053efb67c68ba382e2fcfd1984eb7723
Uiskam <92760953+Uiskam@users.noreply.github.com>
2024-12-22 18:49:04 +0100

    Comment out the "new" filtering.

--- src/main/kotlin/edu/agh/susgame/back/domain/build/GameInitializer.kt
+++ src/main/kotlin/edu/agh/susgame/back/domain/build/GameInitializer.kt
@@ -9,8 +9,8 @@ object GameInitializer {
     fun getGameParams(players: List<Player>): GameConfig {
         val directoryPath = "src/main/resources/game_files/${players.size}"
         val directory = File(directoryPath)
-        var files = directory.listFiles { _, name -> name.endsWith(".json") }
-        files = directory.listFiles { _, name -> name.endsWith(".json") && name.contains("new") }
+        val files = directory.listFiles { _, name -> name.endsWith(".json") }
+        //files = directory.listFiles { _, name -> name.endsWith(".json") && name.contains("new") }
 
 
         val randomFile = if (files != null && files.isNotEmpty()) {
